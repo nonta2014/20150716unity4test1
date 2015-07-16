369868d4b824587050cd2046c42e268ef54d78a4
n_o_n_t_a_2_0_1_4@yahoo.co.jp <nonchang@203.115.215.220.ap.yournet.ne.jp>
2015-07-16 16:21:08 +0900

    first commit.

---
+++ Assets/TestScenes/SphereBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereBehaviour : MonoBehaviour {
+	void Update() {
+	}
+	void Start() {
+		//
+		Debug.Log("テストです。");
+	}
+}
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Unity4.x系のgithubリポジトリテスト
+
+this is my sandbox repository. no useful item here.
+
+テストテスト。
+githubとunityの挙動テストに使ってます。すみません。何もありません。
